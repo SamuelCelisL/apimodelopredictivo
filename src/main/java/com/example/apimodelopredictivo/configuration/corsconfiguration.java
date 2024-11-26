514a54975ab0e7c45647e1a5c717ee2153a920fa
SamuelCelisL <samuelcelislizcano@gmail.com>
2024-11-25 19:44:19 -0500

    se agrego la configuracion de los cors

---
+++ src/main/java/com/example/apimodelopredictivo/configuration/corsconfiguration.java
@@ -0,0 +1,24 @@
+package com.example.apimodelopredictivo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class corsconfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todos los endpoints
+                        .allowedOrigins("http://localhost:3000") // Cambia según tu dominio
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permitir todos los headers
+                        .allowCredentials(true); // Permite enviar cookies o autenticación
+            }
+        };
+    }
+}
--- src/main/java/com/example/apimodelopredictivo/controller/userController.java
+++ src/main/java/com/example/apimodelopredictivo/controller/userController.java
@@ -3,7 +3,6 @@ package com.example.apimodelopredictivo.controller;
 import java.util.Optional;
 import java.util.List;
 
-// import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
